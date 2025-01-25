c190c4e43330d3e9dd0908c223375fc8d3118ab3
Maurício Fedatto <mfedatto@gmail.com>
2025-01-24 23:18:23 -0300

    Simplify Docker Compose execution and ensure Docker service is running in Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,30 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.0"
+  terraform {
+    required_providers {
+      docker = {
+        source  = "kreuzwerker/docker"
+        version = "~> 2.0"
+      }
     }
   }
-}
 
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
-}
+  resource "null_resource" "ensure_docker" {
+    provisioner "local-exec" {
+      command = <<EOT
+        sudo systemctl start docker
+        sudo systemctl enable docker
+      EOT
+    }
+  }
 
-resource "docker_network" "mkhouse_vpc_net" {
-  name   = "mkhouse-vpc-net"
-  driver = "bridge"
-}
+  provider "docker" {
+    host = "unix:///var/run/docker.sock"
+  }
 
-output "network_name" {
-  value = docker_network.mkhouse_vpc_net.name
-}
+  resource "docker_network" "mkhouse_vpc_net" {
+    name   = "mkhouse-vpc-net"
+    driver = "bridge"
+  }
+
+  output "network_name" {
+    value = docker_network.mkhouse_vpc_net.name
+  }
