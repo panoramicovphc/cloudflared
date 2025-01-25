d094e4182e3a78d7af994ce122f9870aba27c06c
Maurício Fedatto <mfedatto@gmail.com>
2025-01-24 23:49:37 -0300

    Add external data source to check for existing Docker network before creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,16 +11,17 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
+data "external" "check_network" {
+  program = ["bash", "${path.module}/check_network.sh", "mkhouse-vpc-net"]
+}
+
 resource "docker_network" "mkhouse_vpc_net" {
+  count  = data.external.check_network.result.exists == "false" ? 1 : 0
   name   = "mkhouse-vpc-net"
   driver = "bridge"
   check_duplicate = true
-
-  lifecycle {
-    ignore_changes = [name]
-  }
 }
 
 output "network_name" {
-  value = docker_network.mkhouse_vpc_net.name
+  value = docker_network.mkhouse_vpc_net[0].name
 }
