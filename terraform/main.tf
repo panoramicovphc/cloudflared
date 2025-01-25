5f7f05b8d3622f9e523752010dfffd3cf63c3a2e
Maurício Fedatto <mfedatto@gmail.com>
2025-01-24 23:24:44 -0300

    Refactor Docker initialization in Terraform to use conditional package installation and remove redundant service checks

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,38 @@
-  terraform {
-    required_providers {
-      docker = {
-        source  = "kreuzwerker/docker"
-        version = "~> 2.0"
-      }
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.0"
     }
   }
+}
 
-  resource "null_resource" "ensure_docker" {
-    provisioner "local-exec" {
-      command = <<EOT
-        sudo systemctl start docker
-        sudo systemctl enable docker
-      EOT
-    }
+resource "null_resource" "ensure_docker" {
+  provisioner "local-exec" {
+    command = <<EOT
+      if command -v yum &> /dev/null; then
+        sudo yum install -y yum-utils
+        sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+        sudo yum install -y docker-ce docker-ce-cli containerd.io
+      elif command -v apt-get &> /dev/null; then
+        sudo apt-get update
+        sudo apt-get install -y docker.io
+      fi
+      sudo systemctl start docker
+      sudo systemctl enable docker
+    EOT
   }
+}
 
-  provider "docker" {
-    host = "unix:///var/run/docker.sock"
-  }
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
 
-  resource "docker_network" "mkhouse_vpc_net" {
-    name   = "mkhouse-vpc-net"
-    driver = "bridge"
-  }
+resource "docker_network" "mkhouse_vpc_net" {
+  name   = "mkhouse-vpc-net"
+  driver = "bridge"
+}
 
-  output "network_name" {
-    value = docker_network.mkhouse_vpc_net.name
-  }
+output "network_name" {
+  value = docker_network.mkhouse_vpc_net.name
+}
