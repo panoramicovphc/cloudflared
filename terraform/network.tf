a23774fa449a7d37275882b6b32d9f9b33505ede
Maurício Fedatto <mfedatto@gmail.com>
2025-01-26 09:28:06 -0300

    Rename null resource for VPC network creation to enhance clarity in Terraform configuration

--- terraform/network.tf
+++ terraform/network.tf
@@ -2,7 +2,7 @@ data "external" "check_vpc_network" {
   program = ["bash", "-c", "docker network inspect ${var.vpc_network_name} >/dev/null 2>&1 && echo '{\"exists\": \"true\"}' || echo '{\"exists\": \"false\"}'"]
 }
 
-resource "null_resource" "create_network" {
+resource "null_resource" "create_vpc_network" {
   count = data.external.check_vpc_network.result.exists == "true" ? 0 : 1
 
   provisioner "local-exec" {
