735ebe65f7729bd6aa094d7155c266610474081f
Maurício Fedatto <mfedatto@gmail.com>
2025-01-27 16:22:08 -0300

    Update dependency for docker_compose_up to rely on docker_compose_pull

--- terraform/compose.tf
+++ terraform/compose.tf
@@ -7,20 +7,11 @@ resource "null_resource" "docker_compose_pull" {
     ]
 }
 
-resource "null_resource" "docker_compose_down" {
-  provisioner "local-exec" {
-    command = "docker compose -f ${var.docker_compose_file} down"
-  }
-  depends_on = [
-      null_resource.create_vpc_network
-    ]
-}
-
 resource "null_resource" "docker_compose_up" {
   provisioner "local-exec" {
     command = "docker compose -f ${var.docker_compose_file} up -d --force-recreate --remove-orphans --build"
   }
-  depends_on = [null_resource.docker_compose_down]
+  depends_on = [null_resource.docker_compose_pull]
 }
 
 resource "null_resource" "docker_image_prune" {
