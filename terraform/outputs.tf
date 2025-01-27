8645176f0cb9d645338b2cc066b90a57fd505268
Maurício Fedatto <mfedatto@gmail.com>
2025-01-27 19:14:37 -0300

    Refactor Cloudflared deployment to use Docker commands and remove Terraform resources

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_network_name" {
-  description = "The name of the Docker network"
-  value       = var.vpc_network_name
-}
-
-output "docker_host" {
-  description = "The Docker host"
-  value       = var.docker_host
-}
